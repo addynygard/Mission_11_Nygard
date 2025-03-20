4bf0dfdf99f7346e3a96011636b4a70fa2ad8e48
addynygard <dragyn.addy@gmail.com>
2025-03-20 14:47:48 -0600

    phase 2 - adding paganation and everything

--- backend/Mission_11_Nygard.API/Controllers/BookController.cs
+++ backend/Mission_11_Nygard.API/Controllers/BookController.cs
@@ -10,15 +10,25 @@ namespace Mission_11_Nygard.API.Controllers
     {
 
         private BookDbContext _bookContext;
-        public BookController(BookDbContext temp) 
+        public BookController(BookDbContext temp)
         {
-            _bookContext = temp; 
+            _bookContext = temp;
         }
 
-        //[HttpGet("AllProjects")] Way of routing so that you can have multiple different apis on the same data (functional vs non functional for the next one)
-        public IEnumerable<Book> GetBooks()
+        //[HttpGet("AllProjects")] // Way of routing so that you can have multiple different apis on the same data (functional vs non functional for the next one)
+        public IActionResult GetBooks(int pageHowMany = 10, int pageNum = 1) // defulat value of 5 if there is nothing in there 
         {
-            return _bookContext.Books.ToList();
+            var something = _bookContext.Books.Skip((pageNum - 1) * pageHowMany).Take(pageHowMany).ToList();
+
+            var totalNumBooks = _bookContext.Books.Count(); // counts the number of books in the database
+
+            var someObject = new
+            {
+                Books = something,
+                TotalNumBooks = totalNumBooks
+            }; 
+
+            return Ok(someObject); // new way to return information that allow us to pass through 2 returns essentially through an object
         }
 
 
--- frontend/src/BookList.tsx
+++ frontend/src/BookList.tsx
@@ -4,36 +4,93 @@ import { Book } from './types/Book';
 function BookList() {
   const [books, setBooks] = useState<Book[]>([]);
 
+  const [pageSize, setpageSize] = useState<number>(10);
+
+  const [pageNum, setpageNum] = useState<number>(1);
+
+  const [totalItems, setTotalItems] = useState<number>(0);
+
+  const [totalPages, setTotalPages] = useState<number>(0);
+
   useEffect(() => {
     const fetchBooks = async () => {
-      const response = await fetch('http://localhost:5117/Book');
+      const response = await fetch(
+        `http://localhost:5117/Book?pageHowMany=${pageSize}&pageNum=${pageNum}`,
+      );
       const data = await response.json();
-      setBooks(data);
+      setBooks(data.books);
+      setTotalItems(data.totalNumBooks);
+      setTotalPages(Math.ceil(totalItems / pageSize));
     };
 
     fetchBooks();
-  }, []);
+  }, [pageSize, pageNum]); // if something changes, you want this function UseEffect to run again!
 
   return (
     <>
       <h1>List of Hilton's Favorite Books</h1>
       <br />
 
+      {/* shows each card with the book's information  */}
       {books.map((b) => (
-        <div id="bookCard">
-          <h3>{b.title}</h3>
-
-          <ul>
-            <li>Author: {b.author}</li>
-            <li>Publisher: {b.publisher}</li>
-            <li>ISBN: {b.isbn}</li>
-            <li>Classification: {b.classification}</li>
-            <li>Category: {b.category}</li>
-            <li>Page Count: {b.pageCount}</li>
-            <li>Price: ${b.price}</li>
-          </ul>
+        <div id="bookCard" className="card" key="b.bookID">
+          <h3 className="card-title">{b.title}</h3>
+          <div className="card-body">
+            <ul className="list-unstyled">
+              <li>Author: {b.author}</li>
+              <li>Publisher: {b.publisher}</li>
+              <li>ISBN: {b.isbn}</li>
+              <li>Classification: {b.classification}</li>
+              <li>Category: {b.category}</li>
+              <li>Page Count: {b.pageCount}</li>
+              <li>Price: ${b.price}</li>
+            </ul>
+          </div>
         </div>
       ))}
+
+      <button disabled={pageNum === 1} onClick={() => setpageNum(pageNum - 1)}>
+        Previous
+      </button>
+
+      {/* (index = i in a for each loop essentially) */}
+      {/* take i + 1, ie 1, 2, 3, 4... */}
+      {/* setting the page number you are on and setting it to that */}
+      {/* cannot click on the button that they are already on (see disabled) */}
+      {[...Array(totalPages)].map((_, index) => (
+        <button
+          key={index + 1}
+          onClick={() => setpageNum(index + 1)}
+          disabled={pageNum === index + 1}
+        >
+          {index + 1}
+        </button>
+      ))}
+
+      {/* creates a next button */}
+      <button
+        disabled={pageNum === totalPages}
+        onClick={() => setpageNum(pageNum + 1)}
+      >
+        Next
+      </button>
+
+      <br />
+      <label>
+        Results per page:
+        <select
+          value={pageSize}
+          onChange={(b) => {
+            setpageSize(Number(b.target.value));
+            setpageNum(1);
+          }}
+        >
+          {/* gives you a dropdown with these values */}
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+        </select>
+      </label>
     </>
   );
 }
